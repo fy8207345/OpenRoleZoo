50fc7f86a845a73b03916ed63c20f11e638f84f9
Feng Yan <2316696377@qq.com>
2022-02-15 13:29:48 +0800

    build

--- craft/build.linux.aarch64.sh
+++ craft/build.linux.aarch64.sh
@@ -22,7 +22,8 @@ cmake "$HOME/.." \
 -DCMAKE_BUILD_TYPE="$BUILD_TYPE" \
 -DPLATFORM="$PLATFORM_TARGET" \
 -DORZ_WITH_OPENSSL="$WITH_SSL" \
--DCMAKE_INSTALL_PREFIX="$INSTALL_DIR"
+-DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
+-DCMAKE_TOOLCHAIN_FILE=/home/fy/SeetaFace6Open/OpenRoleZoo/CMakeLists-ubuntu-x86-to-arm64-cross-compile.txt
 
 make -j4 VERBOSE=1
 make install
